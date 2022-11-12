af1ace0ab73af7b0135e445bbc59448ad2caa843
mgn <matheeusgn21@gmail.com>
2022-11-12 13:19:41 -0300

    add parttners and trending

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import './css/index.css'
 
 const btnMenuMobile = document.querySelector('#btnMenuMobile')
 
-function toggleMenu() {
+function toggleMenu(event) {
    const nav = document.querySelector('#nav')
    const rmvActive = document.querySelector('#rmvActive')
+
+   if (event.type === 'touchstart') event.preventDefault()
 
    nav.classList.toggle('active')
    rmvActive.classList.toggle('activeRmv')
 }
 
 btnMenuMobile.addEventListener('click', toggleMenu)
+btnMenuMobile.addEventListener('touchstart', toggleMenu)
