7e342df64ff8ba3736fdcf7fab17f0b3c233913a
mgn <matheeusgn21@gmail.com>
2022-11-13 17:18:58 -0300

    add trending-nfts

--- src/main.js
+++ src/main.js
@@ -19,3 +19,43 @@ btnMenuMobile.addEventListener('click', toggleMenu)
 btnMenuMobile.addEventListener('touchstart', toggleMenu)
 
 /* --- menu mobile - end --- */
+
+/*------------------------*/
+
+/* --- carousel - trending-nfts - start --- */
+
+const controls = document.querySelectorAll('.area-buttom .control')
+let currentCard = 0
+const card = document.querySelectorAll('.gallery ul .card')
+const maxCards = card.length
+
+controls.forEach(control => {
+   control.addEventListener('click', () => {
+      const isLeft = control.classList.contains('arrow-left')
+
+      if (isLeft) {
+         currentCard -= 1
+      } else {
+         currentCard += 1
+      }
+
+      if (currentCard >= maxCards) {
+         currentCard = 0
+      }
+
+      if (currentCard < 0) {
+         currentCard = maxCards - 1
+      }
+
+      card.forEach(card => card.classList.remove('current-card'))
+
+      card[currentCard].scrollIntoView({
+         inline: 'center',
+         behavior: 'smooth'
+      })
+
+      card[currentCard].classList.add('current-card')
+   })
+})
+
+/* --- carousel - trending-nfts - end --- */
